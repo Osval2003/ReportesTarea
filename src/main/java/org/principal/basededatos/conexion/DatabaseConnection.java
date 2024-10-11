e619c6bacb609665aae48eb9b2b7318489b8ec35
Enner Godoy <ennergodoy@gmail.com>
2024-10-11 17:51:55 -0600

    Initial commit

---
+++ src/main/java/org/principal/basededatos/conexion/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package org.principal.basededatos.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:sqlite:C:\\SqlLite\\sqlite-tools-win-x64-3460100\\db_telebot";
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+
+    public static void main(String[] args) {
+        try (Connection conn = getConnection()) {
+            if (conn != null) {
+                System.out.println("Conexión a SQLite establecida.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
